b2cad492d3fa3788c4a3516753f345391256d5f0
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-01-31 11:06:59 -0800

    add flask-cors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 """Flask app for dessert demo."""
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 # from models import db, connect_db, Dessert
 
 app = Flask(__name__)
+CORS(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///desserts'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SQLALCHEMY_ECHO'] = True
