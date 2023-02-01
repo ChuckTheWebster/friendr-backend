67a4fe0af0af6dc6368368154fa45b652cf829bc
Charlotte Webster <charlotte.shae.webster@gmail.com>
2023-01-31 19:21:12 -0500

    add the User model and seed database

--- app.py
+++ app.py
@@ -2,16 +2,28 @@
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-# from models import db, connect_db, Dessert
+from models import db, connect_db, User
+from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 CORS(app)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///desserts'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///friendr'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 
-# connect_db(app)
+connect_db(app)
 
 @app.get("/")
 def test():
     return "Running"
+
+class UserView(Resource):
+    def get(self):
+        users = User.query.all()
+        return jsonify(users=[u.serialize for u in users])
+
+api.add_resource(UserView, '/users')
+
+
---
+++ models.py
@@ -0,0 +1,161 @@
+# from datetime import datetime
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "/static/images/default-pic.png"
+
+
+class User(db.Model):
+    """User in the system."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    first_name = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    last_name = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    email = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    username = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    location = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    friend_radius = db.Column(
+        db.Integer,
+        nullable=False,
+        default=25
+    )
+
+    image_url = db.Column(
+        db.Text,
+        default=DEFAULT_IMAGE_URL,
+    )
+
+    bio = db.Column(
+        db.Text,
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    # messages = db.relationship('Message', backref="user")
+
+    # followers = db.relationship(
+    #     "User",
+    #     secondary="follows",
+    #     primaryjoin=(Follows.user_being_followed_id == id),
+    #     secondaryjoin=(Follows.user_following_id == id),
+    #     backref="following",
+    # )
+
+    # liked_messages = db.relationship('Message', secondary="likes")
+
+    def __repr__(self):
+        return f"<User #{self.id}: {self.username}, {self.email}>"
+
+    @property
+    def serialize(self):
+        return {"username": self.username,
+            "bio": self.bio,
+            "location": self.location
+        }
+
+    @classmethod
+    def signup(cls, username, email, location, friend_radius, bio, password, first_name, last_name, image_url=DEFAULT_IMAGE_URL):
+        """Sign up user.
+
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            email=email,
+            password=hashed_pwd,
+            image_url=image_url,
+            first_name=first_name,
+            last_name=last_name,
+            location=location,
+            friend_radius=friend_radius,
+            bio=bio
+
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If this can't find matching user (or if password is wrong), returns
+        False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+
+    # def is_followed_by(self, other_user):
+    #     """Is this user followed by `other_user`?"""
+
+    #     found_user_list = [
+    #         user for user in self.followers if user == other_user]
+    #     return len(found_user_list) == 1
+
+    # def is_following(self, other_user):
+    #     """Is this user following `other_use`?"""
+
+    #     found_user_list = [
+    #         user for user in self.following if user == other_user]
+    #     return len(found_user_list) == 1
+
+
+#BOTTOM
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
